5818f012b2c0e653ce692f85b9d001d83dc19c5d
Adhithya Jayakumar <adhithyajayakumar3@gmail.com>
2026-09-25 03:51:06 +0000

    Fix TriangleArea class output

--- TriangleArea.java
+++ TriangleArea.java
@@ -1,13 +1,5 @@
-import java.util.Scanner;
-
 public class TriangleArea {
     public static void main(String[] args) {
-
-        System.out.print("Enter base: ");
-
-        System.out.print("Enter height: ");
-        System.out.println("Area of The Triangle");
-        System.out.println("Area of triangle = " + area);
-
+        System.out.println("Area of the triangle");
     }
 }
